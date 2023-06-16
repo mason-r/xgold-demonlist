9628d6f9b011f41baa8d0a3b243dc1d5074d772c
mason-r <masonrice442@gmail.com>
2023-06-15 21:12:42 -0400

    Added Bloodbath at #1 and added Bloodbath ratings.

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ class DemonRating {
 }
 
 let demons = [
+  69,
   251,
   29,
   new Demon("Reflective", 7108, "Completed", "Mojitoz & more", 78831233, "N/A", "Mojitoz", "Vorgogne", "http://i.ytimg.com/vi/1fzhBT_7Fos/mqdefault.jpg", "https://www.youtube.com/watch?v=1fzhBT_7Fos"),
@@ -38,22 +39,22 @@ let demons = [
   new Demon("Precipitance", 6332, "Completed", "Extravagon", 60197839, "N/A", "Extravagon", "Extravagon", "http://i.ytimg.com/vi/_W9xA5lqmLs/mqdefault.jpg", "https://www.youtube.com/watch?v=_W9xA5lqmLs"),
   new Demon("Acu", 4026, "Completed", "neigefeu", 61079355, "N/A", "neigefeu", "neigefeu", "http://i.ytimg.com/vi/z6l74Mkoxm8/mqdefault.jpg", "https://www.youtube.com/watch?v=z6l74Mkoxm8")
 ];
-let upcomingDemons = [
-  69
-];
+let upcomingDemons = [];
 
 let demonRatings = [
-  new DemonRating(1, "Prismatic Haze", 9.5, 9.5, 8, 8, "Current hardest and current favorite extreme. Always try to play GOOD LEVELS for your hardests!!!"),
-  new DemonRating(2, "Triple Six", 9, 8, 10, 4, "Incredible memory level. Strong epilepsy warning."),
-  new DemonRating(3, "Reflective", 7.5, 5.5, 8, 8.5, "Very annoying predrop, everything else is great."),
-  new DemonRating(4, "Reanimate", 7, 6.5, 8, 9.5, "First extreme demon! The song is the best part, which is usually a red flag, but this song is just so good that it's fine."),
-  new DemonRating(5, "HURRICANE", 7, 8.5, 8, 5, "Littered with chokepoints. All of them are manageable, but still kind of annoying."),
-  new DemonRating(6, "Napalm", 1.5, 3, 7.5, 7, "Current least favorite extreme. I despise this level. A lot of people say it's good, but I just hated it the whole way through."),
-  new DemonRating(7, "Lost Love", 9.5, 9.5, 7.5, 9.5, "Fantastic memory level. I know "),
-  new DemonRating(8, "troll level", 6.5, 8, 6, 7.5, "Predrop is not great, the drop is awesome."),
-  new DemonRating(9, "Retention", 7.5, 8.5, 7, 7, "Has exactly one very annoying chokepoint. Other than that, it's a very fun level."),
-  new DemonRating(10, "Precipitance", 6, 6, 7, 6, "Very average level. This is my easy"),
-  new DemonRating(11, "Acu", 8, 9, 7, 7.5, "Easiest extreme. Somewhat annoying predrop and postdrop, which is the opposite of good balancing.")
+  new DemonRating(1, "Bloodbath", 5, 4, 7, 7.5, "Current hardest and former top 1. Fluked from 52, by far my best fluke to date. This level is very annoyingly balanced, with some parts being \
+    WAY harder than others."),
+  new DemonRating(2, "Prismatic Haze", 9.5, 9.5, 8, 8, "Former hardest and current favorite extreme. Always try to play GOOD LEVELS for your hardests!!!"),
+  new DemonRating(3, "Triple Six", 9, 8, 10, 4, "Incredible memory level. Strong epilepsy warning."),
+  new DemonRating(4, "Reflective", 7.5, 5.5, 8, 8.5, "Very annoying predrop, everything else is great."),
+  new DemonRating(5, "Reanimate", 7, 6.5, 8, 9.5, "First extreme demon! The song is the best part, which is usually a red flag, but this song is just so good that it's fine."),
+  new DemonRating(6, "HURRICANE", 7, 8.5, 8, 5, "Littered with chokepoints. All of them are manageable, but still kind of annoying."),
+  new DemonRating(7, "Napalm", 1.5, 3, 7.5, 7, "Current least favorite extreme. I despise this level. A lot of people say it's good, but I just hated it the whole way through."),
+  new DemonRating(8, "Lost Love", 9.5, 9.5, 7.5, 9.5, "Fantastic memory level. I know "),
+  new DemonRating(9, "troll level", 6.5, 8, 6, 7.5, "Predrop is not great, the drop is awesome."),
+  new DemonRating(10, "Retention", 7.5, 8.5, 7, 7, "Has exactly one very annoying chokepoint. Other than that, it's a very fun level."),
+  new DemonRating(11, "Precipitance", 6, 6, 7, 6, "Very average level. This is my easy"),
+  new DemonRating(12, "Acu", 8, 9, 7, 7.5, "Easiest extreme. Somewhat annoying predrop and postdrop, which is the opposite of good balancing.")
 ]
 
 function generateColorCode(num) {
@@ -183,6 +184,9 @@ $(document).ready(function() {
                 currentDemon.attempts = 6233;
                 currentDemon.creators = "WOOGI1411";
                 break;
+              case "Bloodbath":
+                currentDemon.attempts = 13828;
+                currentDemon.creators = "Riot & more";
             }
             resolve(currentDemon);
           }).fail(function() {
@@ -214,57 +218,62 @@ $(document).ready(function() {
   }
   
   async function upcomingList() {
-    let promises = upcomingDemons.map(function(currentItem) {
-      return new Promise(function(resolve) {
-        let currentDemon;
-        if (typeof currentItem === "number") {
-          $.ajax({
-            url: "https://pointercrate.com/api/v2/demons/" + currentItem + "/",
-            method: "GET"
-          }).done(function(data) {
-            currentDemon = new Demon(
-              data.data.name,
-              null,
-              null,
-              data.data.creators,
-              data.data.level_id,
-              data.data.position,
-              data.data.publisher.name,
-              data.data.verifier.name,
-              data.data.thumbnail,
-              data.data.video
-            );
-            // Clean up the data: replace objects/arrays in creators
-            switch (currentDemon.name) {
-              case "Bloodbath":
-                currentDemon.creators = "Riot & more"
-            }
+    if (upcomingDemons.length > 0) {
+      let promises = upcomingDemons.map(function(currentItem) {
+        return new Promise(function(resolve) {
+          let currentDemon;
+          if (typeof currentItem === "number") {
+            $.ajax({
+              url: "https://pointercrate.com/api/v2/demons/" + currentItem + "/",
+              method: "GET"
+            }).done(function(data) {
+              currentDemon = new Demon(
+                data.data.name,
+                null,
+                null,
+                data.data.creators,
+                data.data.level_id,
+                data.data.position,
+                data.data.publisher.name,
+                data.data.verifier.name,
+                data.data.thumbnail,
+                data.data.video
+              );
+              // Clean up the data: replace objects/arrays in creators
+              switch (currentDemon.name) {
+                case "Bloodbath":
+                  currentDemon.creators = "Riot & more"
+              }
+              resolve(currentDemon);
+            }).fail(function() {
+              console.log("API error");
+              resolve(null); // Resolve with null for failed requests
+            });
+          } else {
+            currentDemon = currentItem;
             resolve(currentDemon);
-          }).fail(function() {
-            console.log("API error");
-            resolve(null); // Resolve with null for failed requests
-          });
-        } else {
-          currentDemon = currentItem;
-          resolve(currentDemon);
+          }
+        });
+      });
+
+      let results = await Promise.all(promises);
+
+      results.forEach(function(currentDemon) {
+        if (currentDemon) {
+          $("#upcoming-container").append(`
+            <div class="demon">
+              <a href=${currentDemon.video} target="_blank"><img src=${currentDemon.thumbnail}></a>
+              <h1>${currentDemon.name}</h1>
+              <h2>By ${currentDemon.creators}</h2>
+              <p>Demon list position: ${currentDemon.position}</p>
+              <p>Projected list placement: <b>1</b></p>
+            </div>`);
         }
       });
-    });
-  
-    let results = await Promise.all(promises);
-  
-    results.forEach(function(currentDemon) {
-      if (currentDemon) {
-        $("#upcoming-container").append(`
-          <div class="demon">
-            <a href=${currentDemon.video} target="_blank"><img src=${currentDemon.thumbnail}></a>
-            <h1>${currentDemon.name}</h1>
-            <h2>By ${currentDemon.creators}</h2>
-            <p>Demon list position: ${currentDemon.position}</p>
-            <p>Projected list placement: <b>1</b></p>
-          </div>`);
-      }
-    });
+    }
+    else {
+      $("#upcoming-container").append(`<p>Nothing right now!</p>`)
+    }
   }
   
   function ratings() {
